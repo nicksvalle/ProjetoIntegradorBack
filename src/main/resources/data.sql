8f3f2afc280d3e4d5735275f7e18f13ce4a0c229
Juliocesargranzotti <jcgranzotti@gmail.com>
2023-11-14 15:34:05 -0300

    Aula com sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 INSERT INTO TBL_PROFESSOR(Professor_name, Professor_email,Professor_course,Professor_date, Professor_materia) VALUES('Will', 'will@gmail.com', 'DS', '20-12-2000', 'Matematica' );  
 INSERT INTO TBL_Sala (Sala_numero,Sala_capacidade,Sala_descricao) VALUES ('12','30 alunos','laboratorio');
 INSERT INTO TBL_Cursos (Curso_name, Curso_descricao, Curso_Semestre) VALUES ('Desenvolvimento De Sistemas','Formaoção em TI','3 anos');
+INSERT INTO TBl_Aulas (Aula_Data, Aula_Horario) Values ('12-12-2021','10:30' )
