386fb30b5ec0dbac927e9541f517cfd28cdc5ba3
Juliocesargranzotti <jcgranzotti@gmail.com>
2023-11-14 15:38:25 -0300

    Exceptions

---
+++ src/main/java/com/projeto_integrador/resources/exceptions/ValidationErrors.java
@@ -0,0 +1,18 @@
+package com.projeto_integrador.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ValidationErrors extends StandardError{
+    private List<String> errors = new ArrayList<>();
+
+    public void addError(String error){
+        this.errors.add(error);
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+}
